fabb42133b3b7a20116456d51786e0a5e38797dc
kresov <kresovivan@mail.ru>
2025-03-12 23:04:15 +0300

    test python1

--- second.py
+++ second.py
@@ -1,2 +1,24 @@
 def function():
     print('second')
+
+
+def print_sum(c, b):
+    print(f"Сумма {c} и {b} равна {c + b}")
+print_sum(10, 15)  # Выведет: Сумма 10 и 15 равна 25
+
+def shout(text):
+    return text.upper()
+
+yell = shout
+
+def greet(func):
+    greeting = func("Hello")  # вызываем функцию
+    print(greeting)
+
+def get_user():
+    name = "Иван"
+    age = 25
+    return name, age
+
+user_name, user_age = get_user()
+print(user_name, user_age)  # Выведет Иван 25
---
+++ tv.py
@@ -0,0 +1,41 @@
+# класс tv
+
+class TV():
+    def __init__(self):
+        self.isOn = False
+        self.isMuted = False
+        # произвольный список каналов по умолчанию
+        self.channelList = [2,4,5,7,9,11,20,36,44,54,65]
+        self.nChannels = len(self.channelList)
+        self.channelIndex = 0
+        self.VOLUME_MINIMUM - 0 # константа
+        self.VOLUME_MAXIMUM = 10 # константа
+        self.volume = self.VOLUME_MAXIMUM #целочисленная переменная
+
+    def power(self):
+        self.isOn = not self.isOn #переключатель
+
+    def volumeUp(self):
+        if not self.isOn:
+            return
+        if self.isMuted:
+            self.isMuted = False #изменение громкости меняет звук, если он отключен
+
+        if self.volume < self.VOLUME_MAXIMUM:
+            self.volume = self.volume + 1
+
+    def volumeDown(self):
+        if not self.isOn:
+            return
+        if self.isMuted:
+            self.isMuted = False #Изменение громкости включает звук, если тот отключен
+
+            if self.volume > self.VOLUME_MINIMUM:
+                self.volume = self.volume - 1
+
+    def channelUp(self):
+        if not self.isOn:
+            return
+        self.channelIndex = self.channelIndex + 1
+        if self.channelIndex > self.nChannels:
+            self.channelIndex = 0 # После последнего канала вернуться к первому каналу
