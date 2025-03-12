e2b82a828f254c4d1933eb6565ec2b6f44bff4ce
kresov <kresovivan@mail.ru>
2025-03-13 00:56:34 +0300

    test python1

---
+++ DimmerSwitch.py
@@ -0,0 +1,24 @@
+#Класс DimmerSwitch
+class DimmerSwitch():
+    def __init__(self):
+         self.switchIsOn = False
+         self.brightness = 0
+
+    def turnOn(self):
+        self.switchIsOn = True
+
+    def turnOff(self):
+        self.switchIsOn = False
+
+    def raiseLevel(self):
+        if self.brightness < 10:
+            self.brightness = self.brightness + 1
+
+    def lowerLevel(self):
+        if self.brightness > 0:
+            self.brightness = self.brightness - 1
+    #Дополнительный метод для отладки
+    def show(self):
+        print('Switch is on?',  self.switchIsOn)
+        print('Brightness is:', self.brightness)
+
