b0b88d7fca62859a068143ec5b936affbc9365dd
kresov <kresovivan@mail.ru>
2025-03-13 00:18:55 +0300

    test python1

--- tv.py
+++ tv.py
@@ -8,7 +8,7 @@ class TV():
         self.channelList = [2,4,5,7,9,11,20,36,44,54,65]
         self.nChannels = len(self.channelList)
         self.channelIndex = 0
-        self.VOLUME_MINIMUM - 0 # константа
+        self.VOLUME_MINIMUM = 0 # константа
         self.VOLUME_MAXIMUM = 10 # константа
         self.volume = self.VOLUME_MAXIMUM #целочисленная переменная
 
@@ -20,7 +20,6 @@ class TV():
             return
         if self.isMuted:
             self.isMuted = False #изменение громкости меняет звук, если он отключен
-
         if self.volume < self.VOLUME_MAXIMUM:
             self.volume = self.volume + 1
 
@@ -28,10 +27,9 @@ class TV():
         if not self.isOn:
             return
         if self.isMuted:
-            self.isMuted = False #Изменение громкости включает звук, если тот отключен
-
-            if self.volume > self.VOLUME_MINIMUM:
-                self.volume = self.volume - 1
+            self.isMuted = False
+        if self.volume > self.VOLUME_MINIMUM:
+            self.volume = self.volume - 1
 
     def channelUp(self):
         if not self.isOn:
@@ -69,4 +67,38 @@ class TV():
         else:
             print('   TV is: Off')
 
+#Создаем объекты телевизоры TV
+oTV = TV()
+oTV1 = TV()
+oTV2 = TV()
+
+#Включаем телевизор и показываем статус
+oTV.power()
+oTV.showInfo()
+
+#Дважды меням канал, дважды увеличиваем громкость, после этого показываем статус
+oTV.channelUp()
+oTV.channelUp()
+oTV.volumeUp()
+oTV.volumeUp()
+oTV.showInfo()
+oTV.setChannel(11)
+oTV.volumeDown()
+oTV.volumeDown()
+oTV.volumeDown()
+oTV.volumeDown()
+oTV.showInfo()
+
+oTV1.power()
+oTV1.showInfo()
+oTV1.setChannel(36)
+oTV1.volumeDown()
+oTV1.showInfo()
+
+oTV2.power()
+oTV2.showInfo()
+oTV2.setChannel(65)
+oTV2.volumeDown()
+oTV2.power()
+oTV2.showInfo()
 
