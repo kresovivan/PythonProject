c6da5dc1e4c47dd0b1fb2d73af7005650a4afdfc
kresov <kresovivan@mail.ru>
2025-03-16 01:33:53 +0300

    test python1

--- window1.py
+++ window1.py
@@ -1,3 +1,18 @@
-import tkinter
-Window = tkinter.Tk()
-Display = tkinter.Label(Window, text='Привет!')
+#Окно с событиями
+from tkinter import *
+
+#Функция для события
+def button1Click():
+    Display.config(text='Это радует!')
+def button2Click():
+    Display.config(text='Это огорчает!')
+
+#Основная программа
+Window = Tk()
+Display = Label(Window, font='Arial 16', text='Привет!')
+Display.pack()  # Добавьте эту строку, чтобы метка отобразилась в окне
+Button1 = Button(Window, text='Хорошо', command=button1Click)
+Button2 = Button(Window, text ='Плохо', command=button2Click)
+Button1.pack()
+Button2.pack()
+Window.mainloop()  # Запуск основного цикла обработки событий
