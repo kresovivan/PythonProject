37f4de47f465520848f42e56fb0dded4ab6dca58
kresov <kresovivan@mail.ru>
2025-05-13 00:08:33 +0300

    test python1

--- fffffff.py
+++ fffffff.py
@@ -9,9 +9,5 @@ if 45 <= Score < 65:
     print("Сойдет(4)")
 if 65 <= Score < 80:
     print("Средне(3)")
-if 80 <= Score < 90:
-    print("Хорошо(2)")
-if 90 <= Score <= 100:
-    print("Очень хорошо (1)")
 if Score > 100 or Score < 0:
     print("Неправильно(0)")
