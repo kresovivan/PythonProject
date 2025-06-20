41a271c9c0834838180d0eade0dfa839474cdd3a
kresov <kresovivan@mail.ru>
2025-06-20 20:40:13 +0300

    test python1

---
+++ lesson1.py
@@ -0,0 +1,8 @@
+Text = "Привет! Кто ты?"
+print(Text)
+Name = input()
+Text = "Так значит, ты - " + Name
+print(Text)
+print("А как у тебя дела?")
+Answer = input()
+print("Я рад, что у тебя дела " + Answer)
