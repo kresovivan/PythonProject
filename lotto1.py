92fd08733253eeef934a539791a342871b3c5bc0
kresov <kresovivan@mail.ru>
2025-06-24 15:30:09 +0300

    test python1

--- lotto1.py
+++ lotto1.py
@@ -1,14 +1,15 @@
 import random
+
 Ball = []
-#все боченки еще не вытащены
+#Все шары еще не вытащены
 for Nr in range(1,50):
     Ball.append(0)
-Case: int = random.randint(1,49)
-#Вытягивается 6 боченков
+Case = random.randint(1,49)
+#Вытягивается 6 шаров
 for Nr in range(6):
-    #поиск неиспользованных шаров
+    #Поиск неиспользованных шаров
     while Ball[Case] == 1:
         Case = random.randint(1,49)
-#Пометка использованного шара вытащенным
-Ball[Case] = 1
-print('№ ' + str(Nr+1) + ' => ' + str(Case))
+        #Пометка использованного шара вытащенным
+    Ball[Case] = 1
+    print("№ " + str(Nr+1) + " => " + str(Case))
