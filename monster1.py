4dbf15d7ebf9492fc9580ed7fd553dc68e0086e3
kresov <kresovivan@mail.ru>
2025-06-25 15:43:37 +0300

    test python1

---
+++ monster1.py
@@ -0,0 +1,12 @@
+class Monster:
+    #Аттрибут
+    Name = "Фрэнки"
+    Character = "Необычный"
+    #Метод
+    def show(self):
+        print("Имя: " + self.Name)
+        print("Особенность: " + self.Character)
+
+#Основная программа
+Frank = Monster()
+Frank.show()
