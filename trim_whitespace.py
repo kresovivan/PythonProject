5c17e1f13c3e323cb5a9c3766efcce054bd012dc
kresov <kresovivan@mail.ru>
2025-06-26 21:47:17 +0300

    test python1

---
+++ trim_whitespace.py
@@ -0,0 +1,12 @@
+favorite_language = 'python   '
+favorite_language = favorite_language.rstrip()
+print(favorite_language)
+
+favorite_language1 = '    python1'
+favorite_language1 = favorite_language1.lstrip()
+print(favorite_language1)
+
+
+favorite_language2 = '    python2    '
+favorite_language2 = favorite_language2.strip()
+print(favorite_language2)
