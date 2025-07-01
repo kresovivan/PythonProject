4dcf7ee64e74aee28fbf30a08dc2c9f9bbeadf55
kresov <kresovivan@mail.ru>
2025-07-01 15:52:40 +0300

    test python1

--- amusement_park.py
+++ amusement_park.py
@@ -1,7 +1,8 @@
 age = 22
 if age < 4:
-    print("Your admission cost is 0")
+    price = 0
 elif age <18:
-    print("Your admission cost is 22")
+    price = 25
 else:
-    print("Your admission cost is 40")
+    price = 40
+print(f"Your admission cost is {price}")
