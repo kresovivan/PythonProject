71b518063e8671e3bfb144172f9d2f42b00fcd39
kresov <kresovivan@mail.ru>
2025-07-03 15:54:27 +0300

    test python1

--- parrot.py
+++ parrot.py
@@ -1,6 +1,8 @@
 prompt = "\nTell me something, and I will repeat it back to you:"
 prompt += "\nEnter 'q' to end the program. "
-message = "\n"
+message = ""
 while message != 'q':
     message = input(prompt)
     print(message)
+if message != 'q':
+   print(message)
