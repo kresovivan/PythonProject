bda3c05f3034949453c4b9c5f273fe5d7bf5e09c
kresov <kresovivan@mail.ru>
2025-07-04 14:32:09 +0300

    test python1

--- greeter.py
+++ greeter.py
@@ -1,7 +1,4 @@
-#name = input("Please enter your name: ")
-#print(f"\nHello, {name}!")
+def greet_user():
+    print("Hello")
 
-prompt = "If you tell us who you are,we can personalize the message you see"
-prompt += "\nWhant is your first name? "
-name = input(prompt)
-print(f"\nHello, {name}")
+greet_user()
